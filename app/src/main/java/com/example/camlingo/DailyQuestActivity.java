bd27c970fd5ae52bfeb700ee3a1a638268d389d4
Frank Obedi <frankmobedi@gmail.com>
2024-10-28 04:32:10 -0400

    fixed activity id issue

--- app/src/main/java/com/example/camlingo/DailyQuestActivity.java
+++ app/src/main/java/com/example/camlingo/DailyQuestActivity.java
@@ -15,7 +15,7 @@ public class DailyQuestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_daily_quest);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.daily_quests), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
