42d082f11ec2e2315f1edd5f79e9221334f631b6
Hamza Tariq <129800852+pakoramaster@users.noreply.github.com>
2024-10-28 18:32:21 -0400

    Added Main Menu

--- app/src/main/java/com/example/camlingo/MainActivity.java
+++ app/src/main/java/com/example/camlingo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.camlingo;
 
 import android.os.Bundle;
+import android.view.Menu;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    };
+
+    public boolean OnCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 }
